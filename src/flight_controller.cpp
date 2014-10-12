d9da58d65116b2a5ae79ce43ad6acfbdfaf96fcf
Ryan Boland <bolandryanm@gmail.com>
2014-10-11 20:47:46 -0400

    something pid something

--- src/flight_controller.cpp
+++ src/flight_controller.cpp
@@ -1,10 +1,25 @@
 #include "flight_controller.h"
 
-FlightController::FlightController() {}
-
 void FlightController::init(RemoteControl remote) {
   rc = remote;
+
+  for (int i = 0; i < NUM_PIDS; i++) {
+    pid_inputs[i] = 0.0;
+    pid_outputs[i] = 0.0;
+    pid_setpoints[i] = 0.0;
+  }
 }
 
 void FlightController::process() {
 }
+
+FlightController::FlightController() :
+   roll_pid(&pid_inputs[PID_ROLL],
+            &pid_outputs[PID_ROLL],
+            &pid_setpoints[PID_ROLL],
+            0.0, 0.0, 0.0, DIRECT),
+   pitch_pid(&pid_inputs[PID_PITCH],
+             &pid_outputs[PID_PITCH],
+             &pid_setpoints[PID_PITCH],
+             0.0, 0.0, 0.0, DIRECT)
+{}
--- src/flight_controller.h
+++ src/flight_controller.h
@@ -2,6 +2,11 @@
 #define flight_controller_h
 
 #include "remote_control.h"
+#include "PID_v1.h"
+
+#define NUM_PIDS 2
+#define PID_ROLL 0
+#define PID_PITCH 1
 
 class FlightController {
   public:
@@ -12,6 +17,11 @@ class FlightController {
 
   private:
     RemoteControl rc;
+    PID roll_pid, pitch_pid;
+
+    double pid_inputs[2];
+    double pid_outputs[2];
+    double pid_setpoints[2];
 };
 
 #endif
